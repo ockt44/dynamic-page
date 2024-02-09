caa8c6b4ce9c573e0d21114dcad6e8c299de3206
ockt44 <133887834+ockt44@users.noreply.github.com>
2024-02-09 23:22:40 +0800

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// List of page URLs
-const pages = ['https://valentine.liked.my.id/2-The%20Enigmatic%20Tale%20of%20Saint%20Valentine.php', 'https://valentine.liked.my.id/3-A%20Pagan%20Festival%20in%20February.php'];
+function executeScript() {
+  return 'https://valentine.liked.my.id/3-A%20Pagan%20Festival%20in%20February.php';
+}
 
-// Get a random index to select a page
-const randomIndex = Math.floor(Math.random() * pages.length);
 
-// Redirect to the randomly selected page
-window.location.href = pages[randomIndex];
+executeScript();
